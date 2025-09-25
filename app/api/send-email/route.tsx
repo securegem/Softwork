bbfb005610bc51201c74eec03a78c3191be9adb6
securegem <securepay416@gmail.com>
2025-09-25 11:22:44 +0100

    Update route.tsx

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -31,7 +31,7 @@ export async function POST(request: Request) {
       apiKey: apiToken,
     })
 
-    const fromEmail = "secure@test-r83ql3pkkmpgzw1j.mlsender.net"
+    const fromEmail = "secure@test-yxj6lj9ow3x4do2r.mlsender.net"
     const sentFrom = new Sender(fromEmail, "Deposit System")
     const recipients = [new Recipient(receiverEmail, "Admin")]
 
